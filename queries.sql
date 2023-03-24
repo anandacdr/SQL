9b49a62c1a2517bdd01d0da9cf047ffc6284ee6d
anandacdr <jazzw815@gmail.com>
2023-03-25 00:33:14 +0545

    queries

---
+++ queries.sql
@@ -0,0 +1,12 @@
+-- Q1
+SELECT * FROM d1;
+
+-- Q2
+SELECT first_name, last_name
+FROM d1
+WHERE hire_date > '2015-01-01';
+
+-- Q3
+SELECT first_name, last_name
+FROM d1
+WHERE hire_date > '2015-01-01'
